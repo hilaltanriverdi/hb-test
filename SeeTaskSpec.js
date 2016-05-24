53c6a366889838b5ea386cdefe7f54f509d0963d
tnrvrd <tanriverdi.hilal@gmail.com>
2016-05-24 17:08:26 +0300

    product seçilmediginde task penceresi görünmemesi senaryosu eklendi

--- SeeTaskSpec.js
+++ SeeTaskSpec.js
@@ -3,8 +3,8 @@
 describe(
     'account', function () {
         browser.driver.manage().window().maximize();
-        
-      
+         var accountMenu = element(by.xpath('//button[contains(., "Cta")]'));
+         var logout = element(by.xpath('//md-menu-content/md-menu-item[3]/button/label'));
 
         beforeAll(
             function () {
@@ -26,11 +26,11 @@ describe(
         var selectoneproduct=element(by.xpath('//tbody/tr[1]/td[1]/md-checkbox/div[1]'));
 
         selectoneproduct.click();
-        browser.sleep(5000);
+        
 
         var taskButton=element(by.xpath('/html/body/div/md-content/md-card/md-card-content/div/div/product-table/div/md-custom-table/div/ng-transclude/ng-transclude/md-toolbar/div/button[2]/span'));
         taskButton.click();
-        browser.sleep(4000);
+        browser.sleep(1000);
         
 
         browser.ignoreSynchronization = false;
@@ -40,26 +40,52 @@ describe(
 
         it('see task for more than one product', function(){
         browser.driver.manage().window().maximize();
-        browser.ignoreSynchronization = true;
-        var exit=element(by.css('/html/body/div[3]/md-dialog/form/md-toolbar/div/button/md-icon'));
-        exit.click();
-        /*browser.sleep(2000);
         console.log('task for more product started'); 
-        var productButton = element.all(by.xpath('//span[contains(text(), "Product")]')).first();
-        productButton.click();
-        browser.sleep(5000);
+        browser.ignoreSynchronization = true;
+
+        var btncancel=element(by.xpath('//button[contains(., "cancel")]'));
+        btncancel.click();
+        browser.sleep(1000);
 
         var selectproducts=element(by.xpath('//thead/tr/th[1]/md-checkbox/div[1]'));
         selectproducts.click();
-        browser.sleep(5000);
 
         var taskButton=element(by.xpath('/html/body/div/md-content/md-card/md-card-content/div/div/product-table/div/md-custom-table/div/ng-transclude/ng-transclude/md-toolbar/div/button[2]/span'));
         taskButton.click();
-        browser.sleep(4000);*/
-    
+        browser.sleep(2000);
 
-        browser.ignoreSynchronization = false;
+        btncancel.click();
+        browser.sleep(2000);
+
+         browser.ignoreSynchronization = false;
         });
+
+
+
+        it('do not see task when did not select product', function(){
+        browser.driver.manage().window().maximize();
+        console.log('do not see task when did not select product started'); 
+        browser.ignoreSynchronization = true;
+        
+        var selectproducts=element(by.xpath('//thead/tr/th[1]/md-checkbox/div[1]'));
+        selectproducts.click();
+
+        expect(element(by.xpath('/html/body/div/md-content/md-card/md-card-content/div/div/product-table/div/md-custom-table/div/ng-transclude/ng-transclude/md-toolbar/div/button[2]/span')).isDisplayed()).toBe(false); 
+        browser.ignoreSynchronization = false;
+        console.log('do not see task when did not select product finished'); 
+        });
+
+
+         afterAll(
+            function () {
+                browser.ignoreSynchronization = true;
+                accountMenu.click();
+                logout.click();
+                browser.ignoreSynchronization = false;
+                browser.sleep(2000);
+                console.log('logout successfully'); 
+            }
+        );
 
 
 
