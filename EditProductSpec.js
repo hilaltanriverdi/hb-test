772d5067d97113c911d02e81d2282bbe4caeff18
tnrvrd <tanriverdi.hilal@gmail.com>
2016-05-24 17:16:01 +0300

    ürün edit edilip cancel butonuna basıldıgında degişiklik olmaması senaryosu eklendi

--- EditProductSpec.js
+++ EditProductSpec.js
@@ -16,7 +16,7 @@ describe(
 
         it('edit product and see revision', function(){
 
-        console.log("edit product spec");
+        console.log("edit product spec started");
         browser.driver.manage().window().maximize();
         browser.ignoreSynchronization = true;
         browser.sleep(2000); 
@@ -28,19 +28,19 @@ describe(
 
         
         product.click();
-        browser.sleep(5000);
+        browser.sleep(3000);
 
       
         var inputField=element.all(by.model('vm.model.fields[key]')).first();
         inputField.sendKeys('yeni');
-        browser.sleep(3000);
+        browser.sleep(2000);
         
         var saveButton=element(by.xpath('//button[contains(., "Save")]'));
         saveButton.click();
         browser.sleep(6000);
         
         product.click();
-        browser.sleep(5000);
+        browser.sleep(2000);
 
         
         expect(element.all(by.model('vm.model.fields[key]')).first().getAttribute('value')).toEqual("yeni");
@@ -48,6 +48,43 @@ describe(
         browser.ignoreSynchronization = false;
          console.log("edit product spec finished");
 });
+
+
+
+         it('cansel to edit product and see there is no revision', function(){
+
+        console.log("cansel to edit product spec started");
+        browser.driver.manage().window().maximize();
+        browser.ignoreSynchronization = true;
+        browser.sleep(2000); 
+        var productButton = element.all(by.xpath('//span[contains(text(), "Product")]')).first();
+        productButton.click();
+        browser.sleep(5000);
+
+        var product=element(by.xpath('//tbody/tr[1]/td[2]/div/div/a'));
+
+        
+        product.click();
+        browser.sleep(3000);
+
+      
+        var inputField=element.all(by.model('vm.model.fields[key]')).first();
+        inputField.sendKeys(' ignore this');
+        browser.sleep(2000);
+        
+        var cancelButton=element(by.xpath('//button[contains(., "Cancel")]'));
+        cancelButton.click();
+        browser.sleep(6000);
+        
+        product.click();
+        browser.sleep(2000);
+
+        expect(element.all(by.model('vm.model.fields[key]')).first().getAttribute('value')).toEqual("yeni");
+
+        browser.ignoreSynchronization = false;
+         console.log("cansel to edit product spec finished");
+});
+
 
 
         
